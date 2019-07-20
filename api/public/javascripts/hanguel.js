f4f1e2e9eff41a989cd2789b2425b939cf5bb3f1
Garyun Young <garyunyoung@gmail.com>
2019-07-20 23:26:20 +0100

    Extract data to hanguel.js file

---
+++ api/public/javascripts/hanguel.js
@@ -0,0 +1,199 @@
+const alphabet = [
+  {
+    hanguel: "ㄱ",
+    romanisation: "g",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㄲ",
+    romanisation: "kk",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㄴ",
+    romanisation: "n",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㄷ",
+    romanisation: "d",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㄸ",
+    romanisation: "tt",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㄹ",
+    romanisation: "r",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅁ",
+    romanisation: "m",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅂ",
+    romanisation: "b",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅅ",
+    romanisation: "s",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅆ",
+    romanisation: "ss",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅇ",
+    romanisation: "ng",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅈ",
+    romanisation: "j",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅉ",
+    romanisation: "jj",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅊ",
+    romanisation: "ch",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅋ",
+    romanisation: "k",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅌ",
+    romanisation: "t",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅍ",
+    romanisation: "p",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅎ",
+    romanisation: "h",
+    type: "consonant"
+  },
+  {
+    hanguel: "ㅏ",
+    romanisation: "a",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅐ",
+    romanisation: "ae",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅑ",
+    romanisation: "ya",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅒ",
+    romanisation: "yae",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅓ",
+    romanisation: "eo",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅔ",
+    romanisation: "e",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅕ",
+    romanisation: "yeo",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅖ",
+    romanisation: "ye",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅗ",
+    romanisation: "o",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅘ",
+    romanisation: "wa",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅙ",
+    romanisation: "wae",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅚ",
+    romanisation: "oe",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅛ",
+    romanisation: "yo",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅜ",
+    romanisation: "u",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅝ",
+    romanisation: "wo",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅞ",
+    romanisation: "we",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅟ",
+    romanisation: "wi",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅠ",
+    romanisation: "yu",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅡ",
+    romanisation: "eu",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅢ",
+    romanisation: "ui",
+    type: "vowel"
+  },
+  {
+    hanguel: "ㅣ",
+    romanisation: "i",
+    type: "vowel"
+  }
+];
+
+module.exports = alphabet
