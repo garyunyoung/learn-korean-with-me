cb8017207b6fc272bbbb714e618fb4caf9dd1846
Garyun Young <garyunyoung@gmail.com>
2019-07-20 19:30:47 +0100

    Fetch random word from Express server

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+const words = ["one", "two", "three", "four"];
+
+function randomWord() {
+  return words[Math.floor(Math.random() * (words.length - 1))];
+}
+
 router.get("/", function(req, res, next) {
-  res.send("API is working");
+  res.send(randomWord());
 });
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 
 export default class App extends React.Component {
@@ -14,16 +13,12 @@ export default class App extends React.Component {
       .then(res => this.setState({ apiResponse: res }));
   }
 
-  componentWillMount() {
-    this.callAPI();
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.apiResponse}</p>
+          <p>word: {this.state.apiResponse}</p>
+          <button onClick={() => this.callAPI()}>fetch</button>
         </header>
       </div>
     );
