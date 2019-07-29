bd4e9753120d765aea277e3b119b599f15523d67
Garyun Young <garyunyoung@gmail.com>
2019-07-29 22:55:55 +0100

    Connect postgres database

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { Client } = require("pg");
+const connectionString = "postgresql://postgres@localhost:5432/hanguel";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  // connect to database
+  const client = new Client({
+    connectionString: connectionString
+  });
+  client.connect();
+  client.query("SELECT NOW()", (err, result) => {
+    console.log(result);
+    client.end();
+    res.send({ title: result.rows[0].now });
+  });
 });
 
 module.exports = router;
