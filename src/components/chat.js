80871554928bcb44fe0ecf726d119a47cb546863
Garyun Young <garyunyoung@gmail.com>
2019-07-31 21:37:12 +0100

    Add basic chat style

--- src/components/chat.js
+++ src/components/chat.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./chat.scss";
 
-export default class App extends React.Component {
+export default class Chat extends React.Component {
   render() {
     return (
       <div>
         <h1>chat</h1>
+        <div className="chat-box">
+          <div className="chat-bot">
+            <div className="chat-bot-response">chat response one</div>
+            <div className="chat-bot-response">chat response two</div>
+            <div className="chat-bot-response">chat response three</div>
+          </div>
+          <div className="user">
+            <div className="user-response">user response one</div>
+          </div>
+        </div>
+
         <ul>
           <li>
             <Link to="/learn">Back</Link>
