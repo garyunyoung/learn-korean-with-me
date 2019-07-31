cffd142c83ae16c152dbfa7f8c243658d1e2345f
Garyun Young <garyunyoung@gmail.com>
2019-07-31 22:33:20 +0100

    Add octopus

--- src/components/play.js
+++ src/components/play.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import octopus from "../images/octopus.svg";
 
 export default class Learn extends React.Component {
   render() {
     return (
       <div>
         <h1>play category</h1>
+        <img src={octopus} width="100" />
         <ul>
           <li>
             <Link to="/play/game">Vowels</Link>
